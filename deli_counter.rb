1f60561123b8a840106c8886499dee9491e5c81a
Henry Allan <henryallan50@gmail.com>
2019-10-30 19:06:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+def line(people_in_queue)
+  
+  if people_in_queue.size < 1
+    
+    puts "The line is currently empty."
+    
+  else
+    
+    count = 1
+    
+    new_array = ["The line is currently:"]
+    
+    people_in_queue.each do |person|
+      
+      new_array.push("#{count}. #{person}")
+      
+      count += 1
+      
+    end
+    
+    new_string = new_array.join(" ") 
+    
+    p new_string
+      
+  end
+  
+end
