cc4cc68701bf4d59daea4320da38375a79c88060
Henry Allan <henryallan50@gmail.com>
2019-10-30 19:39:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -34,16 +34,16 @@ def take_a_number (array, name)
   
 end
 
-def now_serving (meh)
+def now_serving(array)
   
-  if meh.size < 1
+  if array.size < 1
     
     puts ("There is nobody waiting to be served!")
     
   else
     
     puts (array(0))
-    meh.delete_at(0)
+    array.delete_at(0)
     
   end
   
